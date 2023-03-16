bfd8cd0769ed2c9b8dca53a74c1308836157ad92
boylase3@gmail.com <boylase3@gmail.com>
2023-03-16 22:22:39 +0700

    first commit

---
+++ pages/sample-Link/detail.php
@@ -0,0 +1,106 @@
+<html>
+    <head>
+        <meta charset="TTF-8"> 
+        <title>Sampel Link Detail</title>
+
+        <!--link-rel-->
+        <?php include '../templates/link-rel.php'; ?>
+
+    </head>
+    <body class="hold-transition sidebar-mini layout-fixed">
+        <div class="wrapper">
+
+             <!-- // header -->
+            <?php include '../templates/header.php'; ?>
+            <!-- // side menu -->
+            <?php include '../templates/menu.php'; ?>
+       
+            <!-- // content -->
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Main content -->
+                <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                    <div class="col-12">
+                        <div class="card my-3">
+                        <div class="card-header">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h3 class="card-title">Data Sampel Link Detail</h3>
+                                </div>
+                                <div class="col-md-6">
+                                    <a href="index.php" class="btn btn-sm btn-primary float-right">
+                                        <i class="fa fa-arrow-left"></i>&nbsp; Kembali Ke List Data
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- /.card-header -->
+                        <div class="card-body">
+
+                        
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="">Nama</label>
+                                        <p>Yamamoni Lase</p>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                   <div class="form-group">
+                                        <label for="">Umur</label>
+                                        <p>23 Tahun</p>
+                                   </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="">Tanggal Lahir</label>
+                                        <p>01 Januari 2000</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="">Status</label>
+                                        <p>Jomblo</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="">Alamat</label>
+                                        <p>NIAS INDONESIA</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.container-fluid -->
+                </section>
+                <!-- /.content -->
+            </div>
+
+
+            <!--footer-->
+            <?php include '../templates/footer.php' ?>
+            <!-- // script-->
+            <?php include '../templates/script.php'; ?>
+
+        </div>
+    </body>
+</html>
