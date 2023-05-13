df3b3ef7916025f4602a4879e40cd3f71711400c
muhammadmarshal590 <muhammadmarshal590@gmail.com>
2023-05-13 18:47:55 +0700

    first commit

---
+++ pages/mata_pelajaran/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Starter</title>
+
+  <!-- include css -->
+  <?php include '../templates/link-rel.php'; ?>
+  
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+    
+    <!-- include header -->
+    <?php include '../templates/header.php'; ?>
+
+    <!-- include menu -->
+    <?php include '../templates/menu.php'; ?>
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Starter Page</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="m-0">Mata Pelajaran</h5>
+              </div>
+              <div class="card-body">
+                <h6 class="card-title">Special title treatment</h6>
+
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <!-- /.col-md-6 -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+    
+    <!-- include menu -->
+    <?php include '../templates/footer.php'; ?>
+
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- include menu -->
+<?php include '../templates/script.php'; ?>
+
+</body>
+</html>
---
+++ pages/templates/menu.php
@@ -0,0 +1,167 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Alexander Pierce</a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="../../pages/dashboard/" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="../../pages/daftar_karyawan/" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>Daftar Karyawan</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="../../pages/data_pendaftaran_online/" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>Data Pendaftaran</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="../../pages/data_siswa/" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>Data Siswa</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="../../pages/data_alumni/" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>Data Alumni</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="../../pages/rangkuman_nilai/" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>Rangkuman Nilai</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="../../pages/absensi_siswa/" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>Absensi Siswa</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="../../pages/modul_pelajaran/" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>Modul Pelajaran</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="../../pages/keuangan/" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>Keuangan</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="../../pages/daftar_info/" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>Daftar Info</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Master Data
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+           
+               <li class="nav-item">
+                <a href="../../pages/kelas/" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Kelas</p>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="../../pages/mata_pelajaran/" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Mata Pelajaran</p>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="../../pages/jabatan/" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Jabatan</p>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="../../pages/jurusan/" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Jurusan</p>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="../../pages/tahun_akademik/" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Tahun Akademik</p>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="../../pages/kurikulum/" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Kurikulum</p>
+                </a>
+              </li>
+
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
